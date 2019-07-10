4254b0c1ae03dc2e39876e9b35226fac0b4e4173
Micheal Shanahan <m_shanahan@hotmail.com>
2019-07-10 11:04:07 -0600

    Hello Class

---
+++ exercises/hello-class/app.js
@@ -0,0 +1,29 @@
+var sayHello = function(){
+    console.log("hello class");
+}
+// call function
+
+sayHello()
+
+// expression to declaration
+
+function sayHello2(){
+    console.log("hello class");
+}
+
+// lv2 name parameter 
+
+function sayHello3(name){
+    console.log ("Hello " + name);
+}
+
+sayHello3("Mike")
+
+// level 3
+
+function sayHello4(name, age){
+    console.log ("Hello " + name + ", how do you like being " + age + "?");
+}
+
+sayHello4("Mike", 35)
+
