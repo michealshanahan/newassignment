2e6edf0e4abb898a550f2063e088dd8176962ed9
Mike Shanahan <m_shanahan@hotmail.com>
2019-10-08 13:53:26 -0600

    October

--- projects/portfolio/src/About.js
+++ projects/portfolio/src/About.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
+
 const About = () => {
     return(
-        <div className = "about" >
-            I am a web developer with an electronics background. I have experience with JavaScript, HTML, CSS, React, and databases including MongoDB. I also have experience programming highly sensitive electronics. I have great problem solving skills and an eye for detail. Contact me today and I will put these skills to work for you! 
+        <div>
+            <div className = "about" >
+                <img src = {require("./assets/picture.JPG")} className = "image" alt = "photo" /> 
+                <span className = "about-text">
+                    I am a web developer with an electronics background. I have experience with JavaScript, HTML, CSS, React, and databases including MongoDB. I also have experience programming highly sensitive electronics. I have great problem solving skills and an eye for detail. 
+                    <br/>
+                    My user-side experience with large data management programs, such as enterprise resource planning, inventory management systems, and other logistics software gives me a great insight into how data managment should work. I know how frustrating these programs can be, and I also know how to create a great user experience.  
+                    <br/>
+                    <br/>
+                    Contact me today and I will put these skills to work for you! 
+                </span>
+                <span className = "about-text">
+
+                </span>
+
+            </div>
+
         </div>
     )
 }
--- projects/portfolio/src/Resume.js
+++ projects/portfolio/src/Resume.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Resume = () => {
     return (
         <div className = 'resume' >
-            <iframe title = 'resume' className = 'resume-pdf' src="https://drive.google.com/file/d/1bTJP0lIwiNV8C-I9W31J_lzYMiV7EjZo/preview" width="820" height="1150"></iframe>   
+           <iframe src="https://drive.google.com/file/d/1uh8TRe7_BLTWD920JbVtqmC8Es_ZinkM/preview" width="820" height="1200"></iframe>
         </div>
     )
 }
