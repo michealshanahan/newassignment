c46eb739ee8861042fd19e51999ce7de5f84ebcb
Mike Shanahan <m_shanahan@hotmail.com>
2019-12-05 11:51:19 -0700

    Added API request

--- projects/api-project/src/Drink.js
+++ projects/api-project/src/Drink.js
@@ -6,12 +6,7 @@ class Drink extends Component{
     render(){
     let {idDrink, strInstructions, strMeasure1, strMeasure2, strMeasure3, strMeasure4, strMeasure5, strMeasure6, strMeasure7, strMeasure8, strMeasure9, strMeasure10, strMeasure11, strMeasure12, strMeasure13, strMeasure14, strMeasure15, strDrink, strDrinkThumb, strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, strIngredient15} = this.props.drink
     
-    
-    
     const measureArray = [strMeasure1, strMeasure2, strMeasure3, strMeasure4, strMeasure5, strMeasure6, strMeasure7, strMeasure8, strMeasure9, strMeasure10, strMeasure11, strMeasure12, strMeasure13, strMeasure14, strMeasure15]
-    
-  
-
     const ingredientsArray = [strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, strIngredient15]
     const ingredientsMap = ingredientsArray.map((ingredient, index) => {
         if(ingredient){
