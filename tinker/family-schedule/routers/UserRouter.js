ec0aa1fa67aceaae1a297f44a42d1b5cd2af72f5
mshanahan <m_shanahan@hotmail.com>
2020-01-18 21:06:40 -0700

    You:

--- tinker/family-schedule/routers/UserRouter.js
+++ tinker/family-schedule/routers/UserRouter.js
@@ -1,5 +1,17 @@
 const express = require( 'express' )
+const app = express()
 
 const userRouter = express.Router()
 
-userRouter
+userRouter.get('/users', (req, res, next) => {
+    User.find((err, user)=> {
+        if(err) {
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(user)
+    })
+})
+
+
+module.exports = userRouter
