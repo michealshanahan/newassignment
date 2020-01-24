e7760cd99b8264029e3abe230bbf4848b32b9adc
mshanahan <m_shanahan@hotmail.com>
2020-01-23 19:28:12 -0700

    update

--- angular-user-db/src/app/app.module.ts
+++ angular-user-db/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    ProfileComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
