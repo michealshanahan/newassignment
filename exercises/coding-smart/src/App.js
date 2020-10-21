0d36f6205954575ff7ae7a32d542c717d34adecb
mshanahan <m_shanahan@hotmail.com>
2020-10-20 20:29:45 -0600

    yo

--- exercises/coding-smart/src/App.js
+++ exercises/coding-smart/src/App.js
@@ -6,13 +6,18 @@ import CreateTodo from "./components/create-todo.component"
 import EditTodo from "./components/edit-todo.component"
 import TodosList from "./components/todos-list.component"
 
+import logo from "./logo.png"
+
 function App() {
   return (
     <Router>
       <div className= "container">
         
         <nav className="navbar navnar-expand-lg navbar-ligh bg-light">
-
+          <a className="navbar-brand" href="http://mike-shanahan.surge.sh/" >
+            <img src={logo} width="30" height="30" alt="Mike-Shanahan.surge.sh/" />
+          
+          </a>
         </nav>
 
         <Route path="/" exact component={ TodosList } />
@@ -23,4 +28,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; 
